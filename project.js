1b20763f9aa508d7e92c8d76c33538e13ae1c5b7
Hassen ben douissa <134536474+hassenbendouiss@users.noreply.github.com>
2024-02-05 15:02:53 +0100

    Delete project.js

--- project.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync")();
-const deposit = () => {
-  const depositAmount = prompt("Enter a deposit amount: ");
-  const numberDepositAmount = parseFloat(depositAmount);
-
-  if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
-    console.log("invalid deposit amount, try again.");
-  }
-};
